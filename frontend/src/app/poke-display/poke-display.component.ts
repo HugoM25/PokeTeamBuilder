a6ce665f7bf18b279578941e2484a6f3915c7e44
HugoM25 <hddm25@gmail.com>
2022-12-11 22:23:08 +0100

    Feature : multiple pokemons

--- frontend/src/app/poke-display/poke-display.component.ts
+++ frontend/src/app/poke-display/poke-display.component.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { pokeInfos } from '../models/poke-infos.model';
 import { PokeInfosServiceService } from '../services/poke-infos-service.service';
-import { OnInit, Input } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-poke-display',
   templateUrl: './poke-display.component.html',
   styleUrls: ['./poke-display.component.scss']
 })
-export class PokeDisplayComponent implements OnInit {
+export class PokeDisplayComponent {
   @Input() pokeInfos!: pokeInfos; 
 
   constructor(private pokeInfosService: PokeInfosServiceService) { }
 
-  ngOnInit(): void {
-    this.pokeInfos = this.pokeInfosService.getPokeInfosList()[0];
-  }
 }
--- frontend/src/app/services/poke-infos-service.service.ts
+++ frontend/src/app/services/poke-infos-service.service.ts
@@ -9,7 +9,12 @@ export class PokeInfosServiceService {
   constructor() { }
 
   pokelist : pokeInfos[] = [
-    {id: 1, name: 'Bulbasaur', imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'}
+    {id: 1, name: 'Bulbasaur', imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'},
+    {id: 2, name: 'Charmander', imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'},
+    {id: 3, name: 'Squirtle', imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png'},
+    {id: 4, name: 'Lapras', imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/131.png'},
+    {id: 5, name: 'Gengar', imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png'},
+    {id: 6, name: 'Pikachu', imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png'}
   ];
 
   getPokeInfosList() {
